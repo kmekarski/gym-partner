28ae12d5e4200ca24fc7bb6c5a93efa07b7fc152
kmekarski <72306134+kmekarski@users.noreply.github.com>
2023-11-20 20:07:49 +0100

    added firebase, project organization

---
+++ GymPartner/App/GymPartnerApp.swift
@@ -0,0 +1,29 @@
+//
+//  GymPartnerApp.swift
+//  GymPartner
+//
+//  Created by Klaudiusz Mękarski on 20/11/2023.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct GymPartnerApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ GymPartner/Core/Home/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  GymPartner
+//
+//  Created by Klaudiusz Mękarski on 20/11/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("GymPartner")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
