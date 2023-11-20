f5a3e3dd9feefc8d03ad1de3b0f9398f29b0a2d5
kmekarski <72306134+kmekarski@users.noreply.github.com>
2023-11-20 19:42:23 +0100

    Initial Commit

---
+++ GymPartner/GymPartnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymPartnerApp.swift
+//  GymPartner
+//
+//  Created by Klaudiusz Mękarski on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct GymPartnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
